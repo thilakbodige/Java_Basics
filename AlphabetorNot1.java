03da2f2b6bd2fcd7adf14e6c2ec26fb557de9440
Thilak Bodige <104753214+thilakbodige@users.noreply.github.com>
2025-02-20 21:11:29 +0530

    Add files via upload

---
+++ AlphabetorNot1.java
@@ -0,0 +1,18 @@
+class AlphabetorNot1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Program Start");
+		char x = '#';
+		if(x>='A'&&x<='Z')
+		{ 
+	      
+		System.out.println(x + "is Alphapet");
+	}
+	else
+	{
+	System.out.println(x + "is Not Aplhabet");
+	}
+	System.out.println("Program Ends");
+	}
+	}
